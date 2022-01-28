bd83e7b9d6d84fb3fd8586cd1d838b63e8e3549e
VeryOldpApY <dalix.playgamer@gmail.com>
2022-01-28 13:19:11 +0100

    Update Compagny.kt

--- app/src/main/java/com/example/companysearchapp/Compagny.kt
+++ app/src/main/java/com/example/companysearchapp/Compagny.kt
@@ -7,18 +7,15 @@ data class Compagny(var id : Int? = null,
                     var siret : String? = null,
                     var codePostal : String? = null,
 
-                    var numVoie : String? = null,
-                    var typeVoie : String? = null,
-                    var libVoie : String? = null,
+                    //var numVoie : String? = null,
+                    //var typeVoie : String? = null,
+                    //var libVoie : String? = null,
 
-                    var arrondissement : String? = null,
+                    //var arrondissement : String? = null,
                     var libCommune : String? = null,
 
-                    var libRegion : String? = null,
-                    var departement : String? = null,
-
-                    var libActPrin : String? = null,
-                    var catEntre : String? = null)
+                    var libActPrin : String? = null)
+                    //var catEntre : String? = null)
 {
     override fun toString(): String
     {
